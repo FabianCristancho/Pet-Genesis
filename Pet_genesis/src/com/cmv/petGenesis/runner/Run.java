d10189881a42111989c00b7341674bf36e781a5e
FredyNicolasMoreno <fredy.moreno01@uptc.edu.co>
2019-07-06 20:57:30 -0500

    New Commit

--- Pet_genesis/src/com/cmv/petGenesis/runner/Run.java
+++ Pet_genesis/src/com/cmv/petGenesis/runner/Run.java
@@ -1,9 +1,12 @@
 package com.cmv.petGenesis.runner;
 
+import com.cmv.petGenesis.views.MainWindow;
+
 public class Run {
 
 	public static void main(String[] args) {
 		System.out.println("HELLO FRIENDS");
+		new MainWindow();
 	}
 
 }
